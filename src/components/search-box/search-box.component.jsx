b1c31e14d734f303f5d2b84c813e99710f850be3
wildanmujahidin <wildan.mujahidin1@gmail.com>
2022-09-17 19:20:35 +0700

    adding starting project

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,27 @@
+// import { Component } from 'react';
+
+import "./search-box.styles.css";
+
+const SearchBox = ({ className, placeholder, onChangeHandler }) => (
+  <input
+    className={`search-box ${className}`}
+    type='search'
+    placeholder={placeholder}
+    onChange={onChangeHandler}
+  />
+);
+
+// class SearchBox extends Component {
+//   render() {
+//     return (
+//       <input
+//         className={`search-box ${this.props.className}`}
+//         type="search"
+//         placeholder={this.props.placeholder}
+//         onChange={this.props.onChangeHandler}
+//       />
+//     );
+//   }
+// }
+
+export default SearchBox;
